2ff667f67f796bf8bc2b873887a8a1da0c4f3b30
robertp30 <parmelee@gmail.com>
2024-09-06 12:19:35 -0400

    L2-start

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data: function() {
+        return {
+            product: 'Boots',
+            description: 'Nice freakin boots'
+        }
+    }
+})
